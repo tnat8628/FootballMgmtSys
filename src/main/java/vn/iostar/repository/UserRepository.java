7c74f05a4f4b3fcb171fec9f92cef7df348c8ee2
quinni1501 <huongquyen15012004@gmail.com>
2024-12-10 12:51:37 +0700

    Completed Profile

--- src/main/java/vn/iostar/repository/UserRepository.java
+++ src/main/java/vn/iostar/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package vn.iostar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import vn.iostar.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -12,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	User findByEmail(String email);
 
 	User findByPhone(String phone);
+	@Modifying
+    @Query("UPDATE User u SET u.avatar = :avatarPath WHERE u.userId = :userId")
+    void updateAvatar(int userId, String avatarPath);
 }
